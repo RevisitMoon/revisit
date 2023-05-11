260eebe45a737e2b9f6088e53eed71e200983f6c
周世成 <zhoushic@zhongketaiyue.com>
2023-05-11 09:37:35 +0800

    init

---
+++ framework-parent/src/main/java/org/revisit/frameworkparent/FrameworkParentApplication.java
@@ -0,0 +1,13 @@
+package org.revisit.frameworkparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkParentApplication.class, args);
+    }
+
+}
---
+++ framework-parent/src/test/java/org/revisit/frameworkparent/FrameworkParentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.revisit.frameworkparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
